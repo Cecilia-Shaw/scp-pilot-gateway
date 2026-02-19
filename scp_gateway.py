4f1d4a2996891c958dd6b10959e56cf2bbb17e52
Cecilia-Shaw <cecilia.zc.shaw@gmail.com>
2026-02-19 21:04:16 +0800

    fix port binding

--- scp_gateway.py
+++ scp_gateway.py
@@ -69,5 +69,6 @@ def evaluate():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", "5055"))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    port = int(os.environ.get("PORT", 8080))
+    print("Using PORT:", port)
+    app.run(host="0.0.0.0", port=port)
